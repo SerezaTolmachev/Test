0c50768e3fcaf49c111a161df53d3f1127306ad9
SerezaTolmachev <serezatoliacev@gmail.com>
2023-12-09 18:27:49 +0300

    Добавил методы sleep и play в класс Cat.

--- file.py
+++ file.py
@@ -1,9 +1,26 @@
+import time
+
+
 class Cat:
     def __init__(self, name, color, year):
         self.name = name
         self.color = color
         self.year = year
+        self.energy = 100
+
+    def sleep(self):
+        print(f'Кот {self.name} уснул.')
+        time.sleep(3)
+        print(f'Кот {self.name} проснулся.')
+        self.energy = 100
+
+    def play(self):
+        print(f'Ты начал играть с {self.name}.')
+        r = int(input('Сколько раз будешь играть?'))
+        self.energy -= r
 
 
 cat = Cat('Boris', 'black', 7)
+cat.play()
+cat.sleep()
 
