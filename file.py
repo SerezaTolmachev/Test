875e0d236e96f2afe3d990ce0b5cc975dd47f485
SerezaTolmachev <serezatoliacev@gmail.com>
2023-12-09 18:12:52 +0300

    Создал класс Cat

--- file.py
+++ file.py
@@ -1,1 +1,9 @@
-print(1)
+class Cat:
+    def __init__(self, name, color, year):
+        self.name = name
+        self.color = color
+        self.year = year
+
+
+cat = Cat('Boris', 'black', 7)
+
